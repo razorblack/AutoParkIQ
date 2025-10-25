467b1b15cd05371b6b0eb71e1a7cff492c2f3cda
Avinash Maurya <64926215+razorblack@users.noreply.github.com>
2025-10-25 22:34:14 +0530

    Initial Project Setup

---
+++ src/main/java/me/razorblack/autoparkiq/AutoParkIqApplication.java
@@ -0,0 +1,13 @@
+package me.razorblack.autoparkiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoParkIqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoParkIqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/razorblack/autoparkiq/AutoParkIqApplicationTests.java
@@ -0,0 +1,13 @@
+package me.razorblack.autoparkiq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoParkIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
